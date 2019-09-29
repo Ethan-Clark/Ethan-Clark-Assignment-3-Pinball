150108751d2802f1c2fee224a0bc4956e552e467
Ethan-Clark <54416505+Ethan-Clark@users.noreply.github.com>
2019-09-29 03:19:01 -0500

    Add files via upload

---
+++ Scripts/GamesPlayed.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamesPlayed : MonoBehaviour
+{
+
+    private TextMesh text3D;
+    private Renderer textRenderer;
+
+    public int gamescore = 0;
+    public string displaytext = "SCORE";
+
+    // Use this for initialization
+    void Start()
+    {
+        textRenderer = GetComponent<Renderer>();
+        textRenderer.sortingLayerName = "Top";
+        text3D = (TextMesh)gameObject.GetComponent(typeof(TextMesh));
+    }
+
+    void Update()
+    {
+        text3D.text = displaytext + ": " + gamescore;
+    }
+}
+
---
+++ Scripts/SliderValue.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using UnityEngine.UI;
+
+
+
+
+
+public class SliderValue : MonoBehaviour
+
+{
+
+    public float speed = 10f;
+
+
+
+    // Update is called once per frame
+
+    void Update()
+
+    {
+
+        transform.Rotate(0, speed * Time.deltaTime, 0);// float with two decimals
+     
+
+    }
+    public void AdjustSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+
+}
